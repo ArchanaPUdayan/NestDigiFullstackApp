3933a97e3e4254110acbc66c6d0cab4df9d69d4a
Archanapudayan <atchanaudayan64534@gmail.com>
2022-12-15 21:32:37 +0530

    Employee

---
+++ src/test/java/com/example/nestdigiFullApp/NestdigiFullAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nestdigiFullApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestdigiFullAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
